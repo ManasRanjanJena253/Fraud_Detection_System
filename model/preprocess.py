1454e4bd638c50d62c41aeb57ea788ed450e042c
Manas Ranjan Jena <mranjanjena253@gmail.com>
2025-07-18 11:04:52 +0530

    Fully trained ML model

--- model/preprocess.py
+++ model/preprocess.py
@@ -26,7 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, strat
 
 test_data = np.concatenate((X_test, y_test.to_numpy().reshape(-1, 1)), axis = -1, )
 test_df = pd.DataFrame(test_data, columns = data.columns.tolist())
-test_df.to_csv("data/test_data.csv")
+test_df.to_csv("data/test_data.csv", index = False)
 
 std_scaler = StandardScaler()
 X_scaled = std_scaler.fit_transform(X_train)
@@ -38,7 +38,7 @@ X_res, y_res = sm.fit_resample(X_scaled, y_train)
 
 all_data = np.concatenate((X_res, y_res.to_numpy().reshape(-1, 1)), axis = 1)
 data_res = pd.DataFrame(all_data, columns = data.columns.tolist())
-data_res.to_csv("data/resampled_train_data.csv")
+data_res.to_csv("data/resampled_train_data.csv", index = False)
 
 # Checking the data distribution after resampling
 sns.displot(data_res["Class"])
